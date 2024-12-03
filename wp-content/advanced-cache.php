7aaef4571d83348def808a96a229d3a0bee39609
devtuanamit1409 <nguyentuanamit@gmail.com>
2024-12-03 09:33:41 +0700

    update

--- wp-content/advanced-cache.php
+++ wp-content/advanced-cache.php
@@ -8,9 +8,9 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'WP_ROCKET_ADVANCED_CACHE', true );
 
-$rocket_path        = '/home/clubart/public_html/wp-content/plugins/wp-rocket/';
-$rocket_config_path = '/home/clubart/public_html/wp-content/wp-rocket-config/';
-$rocket_cache_path  = '/home/clubart/public_html/wp-content/cache/wp-rocket/';
+$rocket_path        = 'D:\cgi-bin\wp-content\plugins\wp-rocket/';
+$rocket_config_path = 'D:\cgi-bin/wp-content/wp-rocket-config/';
+$rocket_cache_path  = 'D:\cgi-bin/wp-content/cache/wp-rocket/';
 
 if (
 	version_compare( phpversion(), '7.3', '<' )
@@ -23,8 +23,8 @@ if (
 }
 
 
-if ( file_exists( '/home/clubart/public_html/wp-content/plugins/wp-rocket/inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php' ) && ! class_exists( 'WP_Rocket_Mobile_Detect' ) ) {
-	include_once '/home/clubart/public_html/wp-content/plugins/wp-rocket/inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+if ( file_exists( 'D:\cgi-bin\wp-content\plugins\wp-rocket/inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php' ) && ! class_exists( 'WP_Rocket_Mobile_Detect' ) ) {
+	include_once 'D:\cgi-bin\wp-content\plugins\wp-rocket/inc/classes/dependencies/mobiledetect/mobiledetectlib/Mobile_Detect.php';
 }
 
 
